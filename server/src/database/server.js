f8277c5d0d42177c3f1af2fc87cadcee5ad9ac66
Mr.Rahul Birajdar <rbirajadar96@gmail.com>
2026-06-21 19:44:07 +0530

    add file

---
+++ server/src/database/server.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "../constant.js";
+
+const app = express();
+
+const connectDB = async () => {
+    try {
+        // const connectionInstant = await mongoose.connect(`${process.env.MONGOODB_URL}/${DB_NAME}`)
+        const connectionInstant = await mongoose.connect("mongodb+srv://rahul:rahul143@cluster0.hrqdiqy.mongodb.net/?appName=Cluster0")
+        console.log(`MongoDB connected !! DB host On ${connectionInstant.connection.host}`)
+    } catch (error) {
+        console.log("ERROR : ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
